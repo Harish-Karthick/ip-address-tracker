c274b1b93a87544d62bbd81e2553ef4bb10e2f93
Harish Karthik <hhkk0010@outlook.com>
2021-01-07 12:54:05 +0530

    added resources for the project based on frontend mentor style guide

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles/rubik-fonts.css";
 import "./index.css";
 
 ReactDOM.render(<h1>Blank App</h1>, document.getElementById("root"));
